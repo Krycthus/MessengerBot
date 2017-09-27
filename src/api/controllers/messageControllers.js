54bfbeb625233dd370e4d6fb860a1e8f05863e0a
Cyril BOURIEZ <cyril.bouriez@epsi.fr>
2017-09-27 16:54:42 +0200

    ajout d'api ai

--- src/api/controllers/messageControllers.js
+++ src/api/controllers/messageControllers.js
@@ -1,5 +1,7 @@
 'use strict'
 const request = require('request');
+const apiaiApp = require('apiai')(process.env.APIAI_TOKEN);
+console.log(process.env.APIAI_TOKEN)
 
 exports.getAuth = function(req, res){
   if (req.query['hub.mode'] && req.query['hub.verify_token'] === process.env.FB_VERIFY) {
@@ -28,19 +30,34 @@ function sendMessage(event) {
   let sender = event.sender.id;
   let text = event.message.text;
 
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: process.env.FB_TOKEN},
-    method: 'POST',
-    json: {
-      recipient: {id: sender},
-      message: {text: text}
-    }
-  }, function (error, response) {
-    if (error) {
-        console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-        console.log('Error: ', response.body.error);
-    }
+  let apiai = apiaiApp.textRequest(text, {
+    sessionId: 'tabby_cat' // use any arbitrary id
   });
+
+  apiai.on('response', (response) => {
+    // Got a response from api.ai. Let's POST to Facebook Messenger
+    let aiText = response.result.fulfillment.speech;
+
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: process.env.FB_TOKEN},
+      method: 'POST',
+      json: {
+        recipient: {id: sender},
+        message: {text: aiText}
+      }
+    }, (error, response) => {
+      if (error) {
+          console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error);
+      }
+    });
+  });
+
+  apiai.on('error', (error) => {
+    console.log(error);
+  });
+
+  apiai.end();
 }
